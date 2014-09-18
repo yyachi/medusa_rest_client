b68fe73a9593a3db631a7990b61d869b6f8a6655
Yusuke Yachi <yusuke.yachi@gmail.com>
2014-09-18 18:45:44 +0900

    box home pwd

--- lib/medusa_rest_client/box.rb
+++ lib/medusa_rest_client/box.rb
@@ -9,10 +9,7 @@ module MedusaRestClient
 			end
 			path = Pathname.new(path)
 			unless path.absolute?
-				puts "#{path} is not absolute ->"
-				p self.pwd
 				path = Pathname.new(self.pwd) + path
-				puts " #{path}"
 			end
 
 			path = path.cleanpath
@@ -31,25 +28,9 @@ module MedusaRestClient
 		end
 
 		def self.chdir(path)
-			p "chdir to #{path}"
-			# unless path
-			# 	path = self.home
-			# 	p self.home
-			# end
-			# path = Pathname.new(path).cleanpath
-			# dirname = path.dirname
-			# basename = path.basename
-			# target_path = dirname.to_s
-			# if dirname == Pathname.new("/")
-			# 	target_path = ""
-			# end
-			# target_name = basename.to_s
-			# box = Box.find(:first, :params => {:q => {:path_eq => target_path, :name_eq => target_name, :m => 'and'}})
 			if box = self.find_by_path(path)
-				p box
-				#@@pwd = path
 				self.pwd_id = box.global_id
-				#@@pwd = path
+				@@pwd = path
 				return true
 			else
 				return false
